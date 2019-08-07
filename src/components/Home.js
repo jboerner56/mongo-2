855d5d7eac22d8baafe72ee5b07a188da38336f1
Jason Boerner <jboerner56@gmail.com>
2019-08-07 11:43:41 -0400

    rewrite beginning of css to individual files for each component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Expenses from './Expenses';
 import Balance from './Balance';
+import style from '../styles/Home.module.css';
 class Home extends React.Component {
     render() {
         return (
-            <div className="page-Content">
-                <div className="home-Title">
+            <div className={style.homeContent}>
+                <div className={style.homeTitle}>
                     <h1>
                         Budget
                     </h1>
