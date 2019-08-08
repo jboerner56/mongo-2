5db3db27fdf87b3937c2c751219c25087f013133
Jason Boerner <jboerner56@gmail.com>
2019-08-08 11:37:40 -0400

    sample dataset before building backend

---
+++ src/Data/data.js
@@ -0,0 +1,8 @@
+let dataSet = [
+    {amount: -20, description: 'Dominoes'},
+    {amount: 500, description: 'Paycheck'},
+    {amount: -50, description: 'Amazon'},
+    {amount: -200, description: 'Supplements'},
+];
+
+export default dataSet;
